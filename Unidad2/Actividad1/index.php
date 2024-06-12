d9d7cfcbd09cddb986859775153000ef6700b2e2
GalaxyUGC <136385655+GalaxyUGC@users.noreply.github.com>
2024-06-12 09:36:23 -0500

    Distribución Mejorada
    Se cambió la Distribución de los archivos en carpetas separadas para una mejor localización y lectura de archivos

---
+++ Unidad2/Actividad1/index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Presentacion</title>
+</head>
+<body>
+<div class="p1">
+    <div class="back1">
+        
+
+
+    </div>
+</div>
+
+<style>
+    body {
+        background: url(https://wallpaperwaifu.com/wp-content/uploads/2022/01/cyberpunk-2077-night-city-thumb.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        margin: 0;
+        height: 100vh;
+    }
+
+    .back1{
+        padding: 5% 15%;
+        background-color: gray;
+        background-position: center;
+        background-repeat: repeat;
+        background-origin: padding-box;
+        margin: 0px 25%;
+    }
+
+    .p1 {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 15px;
+        text-align: left;
+    }
+</style>
+
+</body>
+</html>
